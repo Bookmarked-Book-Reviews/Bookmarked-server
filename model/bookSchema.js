aeb1a7aeffc78591fc7ea6ef897711a2deaab570
Aswin <cgaswin333@gmail.com>
2021-05-25 07:51:54 +0530

    changed bookschema values

--- model/bookSchema.js
+++ model/bookSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const path = require('path')
 
-// const coverImageBasePath = 'uploads/bookCovers'
+
 
 const bookSchema = new mongoose.Schema({
     isbnNumber: {
@@ -21,23 +21,6 @@ const bookSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    coverImageName: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true,
-        unique: true [{
-            validate:{
-                validator: value => validator.isURL(value, { protocols: ['http', 'https', 'ftp'],
-                 require_tld: true,
-                  require_protocol: true}),
-                message: 'Must be a valid URL'
-                    }
-                  }]
-       
-    },
     yearOfPublication: {
         type:Number,
         required: true
@@ -48,11 +31,6 @@ const bookSchema = new mongoose.Schema({
     },
 })
 
-bookSchema.virtual("coverImagePath").get(function(){
-    if(this.coverImageName != null) {
-        return path.join('/', coverImageBasePath, this.coverImageName);
-    }
-})
+
 
 module.exports = mongoose.model('Book', bookSchema)
-// module.exports.coverImageBasePath = coverImagePath;
